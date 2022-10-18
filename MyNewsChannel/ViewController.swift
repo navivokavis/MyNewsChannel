a85b4eb685433149b0e3e062884359cf0016460a
navivokavis <navivokavis@gmail.com>
2022-10-18 09:07:21 +0200

    Initial Commit

---
+++ MyNewsChannel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyNewsChannel
+//
+//  Created by Navi Vokavis on 18.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
