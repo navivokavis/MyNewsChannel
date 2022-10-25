ac2e0b12ff8a079755f14b16bfb1066961e9e7d9
navivokavis <navivokavis@gmail.com>
2022-10-25 12:53:39 +0200

    search don't work

--- MyNewsChannel/ViewControllers/MainViewController.swift
+++ MyNewsChannel/ViewControllers/MainViewController.swift
@@ -10,17 +10,6 @@ import SafariServices
 
 
 class MainViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
- 
-    var categoryMenu: UIMenu {
-        let menuActions = NewsType.allCases.map({ (item) -> UIAction in
-            let name = item.rawValue
-            return UIAction(title: name.capitalized, image: UIImage(systemName: item.systemName)) { (_) in
-                self.categoryMenu.image?.withTintColor(.white, renderingMode: .alwaysTemplate)
-                self.searhNewsByType(type: item)
-            }
-        })
-        return UIMenu(title: "Change Category", children: menuActions)
-    }
     
     let newsTableView = UITableView()
     private var viewModels = [NewsTableViewCellViewModel]()
@@ -34,13 +23,31 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
     //    var dataSource = [String]()
     let vc = SearchViewController()
     
-//    override func viewWillAppear(_ animated: Bool) {
-//        present(SplashViewController(), animated: true)
-//    }
+    //создаю меню
+    var categoryMenu: UIMenu {
+        let menuActions = NewsType.allCases.map({ (item) -> UIAction in
+            let name = item.rawValue
+            return UIAction(title: name.capitalized, image: UIImage(systemName: item.systemName)) { (_) in
+                self.categoryMenu.image?.withTintColor(.white, renderingMode: .alwaysTemplate)
+                self.searhNewsByType(type: item)
+            }
+        })
+        return UIMenu(title: "Change Category", children: menuActions)
+    }
+    
+    //    override func viewWillAppear(_ animated: Bool) {
+    //        present(SplashViewController(), animated: true)
+    //    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
+        
+        vc.completion = { [weak self] text in
+            guard let self = self else { return }
+                    self.searhNewsByWord(title: text)
+                    print("!@UASKLFHASOKFJASLKFHJASLJDHWJKQGEOI!EH(IUOPHNASLJKFH")
+                }
     }
     
     func setup() {
@@ -56,15 +63,13 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
     func configureSubviews() {
         title = "Top Rated News"
         
-        view.backgroundColor = .systemBackground
-        
         self.navigationController?.setupNavigationController()
         self.navigationController!.navigationBar.tintColor = .white
         
         searchRightButton = createCustomBarButton(imageName: "magnifyingglass", selector: #selector(searchRightButtonTapped))
         lineLeftButton = UIBarButtonItem(systemItem: .bookmarks, primaryAction: nil, menu: categoryMenu)
         backbutton = createCustomBarButton(imageName: "arrowshape.turn.up.backward", selector: #selector(backbuttonTapped))
-       
+        
         navigationItem.rightBarButtonItem = searchRightButton
         navigationItem.leftBarButtonItems = [lineLeftButton, backbutton]
         backbutton.customView?.isHidden = true
@@ -95,49 +100,43 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
             }
         }
         
-//
-//        vc.completion = { [weak self] text in
-//            searhNewsByWord(title: text)
-//
-        
-        
-        
+       
     }
     
-    func addTransparentView() {
-        let window = UIApplication.shared.keyWindow
-        darkBacgroundView.frame = window?.frame ?? self.view.frame
-        view.addSubview(darkBacgroundView)
-        
-        transparentView.frame = CGRect(x: 0, y: 150, width: 300, height: 0)
-        view.addSubview(transparentView)
-        transparentView.layer.cornerRadius = 20
-        
-        darkBacgroundView.backgroundColor = .black.withAlphaComponent(0.9)
-
-        let tapgesture = UITapGestureRecognizer(target: self, action: #selector(removeTransparentView))
-        darkBacgroundView.addGestureRecognizer(tapgesture)
-        darkBacgroundView.alpha = 0
-        UIView.animate(withDuration: 0.4, delay: 0.0, usingSpringWithDamping: 1.0, initialSpringVelocity: 1.0, options: .curveEaseInOut, animations: {
-            self.darkBacgroundView.alpha = 0.5
-            self.transparentView.frame = CGRect(x: 20, y: 150, width: 300, height: CGFloat(self.transparentView.dataSource.count * 40))
-            self.transparentView.alpha = 0.9
-        }, completion: nil)
-        
-        // здесь я реализую замыкания
-        transparentView.typeSelect = { [weak self] type in
-            print(type)
-            self?.searhNewsByType(type: type)
-            self!.removeTransparentView()
-        }
-    }
-    
-    @objc func removeTransparentView() {
-        UIView.animate(withDuration: 0.4, delay: 0.0, usingSpringWithDamping: 1.0, initialSpringVelocity: 1.0, options: .curveEaseInOut, animations: {
-            self.darkBacgroundView.alpha = 0
-            self.transparentView.frame = CGRect(x: 20, y: 150, width: 300, height: 0)
-        }, completion: nil)
-    }
+//    func addTransparentView() {
+//        let window = UIApplication.shared.keyWindow
+//        darkBacgroundView.frame = window?.frame ?? self.view.frame
+//        view.addSubview(darkBacgroundView)
+//
+//        transparentView.frame = CGRect(x: 0, y: 150, width: 300, height: 0)
+//        view.addSubview(transparentView)
+//        transparentView.layer.cornerRadius = 20
+//
+//        darkBacgroundView.backgroundColor = .black.withAlphaComponent(0.9)
+//
+//        let tapgesture = UITapGestureRecognizer(target: self, action: #selector(removeTransparentView))
+//        darkBacgroundView.addGestureRecognizer(tapgesture)
+//        darkBacgroundView.alpha = 0
+//        UIView.animate(withDuration: 0.4, delay: 0.0, usingSpringWithDamping: 1.0, initialSpringVelocity: 1.0, options: .curveEaseInOut, animations: {
+//            self.darkBacgroundView.alpha = 0.5
+//            self.transparentView.frame = CGRect(x: 20, y: 150, width: 300, height: CGFloat(self.transparentView.dataSource.count * 40))
+//            self.transparentView.alpha = 0.9
+//        }, completion: nil)
+//
+//        // здесь я реализую замыкания
+//        transparentView.typeSelect = { [weak self] type in
+//            print(type)
+//            self?.searhNewsByType(type: type)
+//            self!.removeTransparentView()
+//        }
+//    }
+//
+//    @objc func removeTransparentView() {
+//        UIView.animate(withDuration: 0.4, delay: 0.0, usingSpringWithDamping: 1.0, initialSpringVelocity: 1.0, options: .curveEaseInOut, animations: {
+//            self.darkBacgroundView.alpha = 0
+//            self.transparentView.frame = CGRect(x: 20, y: 150, width: 300, height: 0)
+//        }, completion: nil)
+//    }
     
     
     
@@ -151,40 +150,42 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
     
-    //MARK: - selector button tapped
+    //MARK: - Search Button Tapped
     @objc func searchRightButtonTapped() {
         self.navigationController?.pushViewController(SearchViewController(), animated: true)
-
-//        presentSearchAlertController(withTitle: "Search", message: "with keyword", style: .alert) { [weak self] keyWord in
-//            APICaller.shared.searchStories(title: keyWord){ [weak self] result in
-//
-//                switch result {
-//                case.success(let articles):
-//                    self?.articles = articles
-//                    self?.viewModels = articles.compactMap({
-//                        NewsTableViewCellViewModel(
-//                            title: $0.title,
-//                            subtitle: $0.description ?? "No Description",
-//                            imageURL: URL(string: $0.urlToImage ?? ""),//может сюда вставляется дефолтная картинка?
-//                            published: $0.publishedAt
-//                        )
-//                    })
-//
-//                    DispatchQueue.main.async { [self]
-//                        self?.newsTableView.reloadData()
-//                        self?.title = "News with '\(keyWord)'"
-//                        self?.backbutton.customView?.isHidden = false
-//                    }
-//                case.failure( let error ):
-//                    print("I have Error \(error)")
-//                }
-//            }
-//        }
+        
+        //здерь реализован метод поиска через ALERT CONTROLLER
+        
+        //        presentSearchAlertController(withTitle: "Search", message: "with keyword", style: .alert) { [weak self] keyWord in
+        //            APICaller.shared.searchStories(title: keyWord){ [weak self] result in
+        //
+        //                switch result {
+        //                case.success(let articles):
+        //                    self?.articles = articles
+        //                    self?.viewModels = articles.compactMap({
+        //                        NewsTableViewCellViewModel(
+        //                            title: $0.title,
+        //                            subtitle: $0.description ?? "No Description",
+        //                            imageURL: URL(string: $0.urlToImage ?? ""),//может сюда вставляется дефолтная картинка?
+        //                            published: $0.publishedAt
+        //                        )
+        //                    })
+        //
+        //                    DispatchQueue.main.async { [self]
+        //                        self?.newsTableView.reloadData()
+        //                        self?.title = "News with '\(keyWord)'"
+        //                        self?.backbutton.customView?.isHidden = false
+        //                    }
+        //                case.failure( let error ):
+        //                    print("I have Error \(error)")
+        //                }
+        //            }
+        //        }
     }
     
     func searhNewsByWord(title: String) {
+        
         APICaller.shared.searchStories(title: title){ [weak self] result in
-            
             switch result {
             case.success(let articles):
                 self?.articles = articles
@@ -198,17 +199,17 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
                 })
                 
                 DispatchQueue.main.async {
-                    print("HOGFKKERFOFKWOFKPWE!!!!!!!!!!!!!!2131231414")
-                    MainViewController().newsTableView.reloadData()
-                    MainViewController().title = "\(title) news"
-                    MainViewController().backbutton.customView?.isHidden = false
+                    print("-123ggg------------HOGFKKERFOFKWOFKPWE!!!!!!!!!!!!!!2131231414")
+                    self?.newsTableView.reloadData()
+                    self?.title = "\(title) news"
+                    self?.backbutton.customView?.isHidden = false
                 }
                 
             case.failure( let error ):
                 print("I have Error \(error)")
             }
         }
-}
+    }
     
     func searhNewsByType(type: NewsType) {
         APICaller.shared.searchStories(title: type.rawValue){ [weak self] result in
@@ -238,13 +239,7 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
     @objc func lineLeftButtonTapped() {
-        
-//        let menuInteraction = UIContextMenuInteraction(delegate: self)
-//        lineLeftButton.customView?.addInteraction(menuInteraction)
-        
-//        lineLeftButton.showsMenuAsPrimaryAction = true
-//        lineLeftButton.menu = menu
-//        addTransparentView()
+        //        addTransparentView()
     }
     
     @objc func backbuttonTapped() {
@@ -284,7 +279,7 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    
+        
         guard let cell = tableView.dequeueReusableCell(
             withIdentifier: NewsTableViewCell.identifier,
             for: indexPath)
--- MyNewsChannel/ViewControllers/SearchViewController.swift
+++ MyNewsChannel/ViewControllers/SearchViewController.swift
@@ -9,29 +9,27 @@ import UIKit
 
 class SearchViewController: UIViewController, UISearchBarDelegate {
     
-    var searchBar = UISearchBar()
-    var searchController = UISearchController()
-    var txt = ""
+    public var completion: ((String) -> Void)?
     private var viewModels = [NewsTableViewCellViewModel]()
     private var articles = [Article]()
-//    public var completion: ((String) -> Void)
-
-
+    var txt = ""
+    var searchBar = UISearchBar()
+    var searchController = UISearchController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .black
-//        view.alpha = 0.7
         title = "Search"
         
         navigationItem.searchController = searchController
         searchController.searchBar.delegate = self
         navigationController?.navigationBar.barStyle = .black
-//        navigationItem.titleView = searchBar
+        //        navigationItem.titleView = searchBar
         
         searchBar.showsCancelButton = true
-//        searchBar.searchTextField.addTarget(self, action: #selector(searchTapped), for: .touchUpInside)
+        //        searchBar.searchTextField.addTarget(self, action: #selector(searchTapped), for: .touchUpInside)
         
-//        searchBar.becomeFirstResponder()
+        searchBar.becomeFirstResponder()
         
         
         
@@ -40,7 +38,7 @@ class SearchViewController: UIViewController, UISearchBarDelegate {
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         print(searchText)
     }
-
+    
     func searchBarShouldBeginEditing(_ searchBar: UISearchBar) -> Bool {
         becomeFirstResponder()
         return true
@@ -48,41 +46,44 @@ class SearchViewController: UIViewController, UISearchBarDelegate {
     
     
     func searchBarTextDidEndEditing(_ searchBar: UISearchBar) {
-        txt = searchBar.searchTextField.text!
-//        self.completion(txt)
-        searhNewsByWord(title: txt)
-        self.navigationController?.popViewController(animated: true)
-}
-
-    func searhNewsByWord(title: String) {
-        APICaller.shared.searchStories(title: title){ [weak self] result in
-            
-            switch result {
-            case.success(let articles):
-                self?.articles = articles
-                self?.viewModels = articles.compactMap({
-                    NewsTableViewCellViewModel(
-                        title: $0.title,
-                        subtitle: $0.description ?? "No Description",
-                        imageURL: URL(string: $0.urlToImage ?? ""),//может сюда вставляется дефолтная картинка?
-                        published: $0.publishedAt
-                    )
-                })
-                
-                DispatchQueue.main.async {
-                    print("HOGFKKERFOFKWOFKPWE!!!!!!!!!!!!!!2131231414")
-                    MainViewController().newsTableView.reloadData()
-                    MainViewController().title = "\(title) news"
-                    MainViewController().backbutton.customView?.isHidden = false
-                }
-                
-            case.failure( let error ):
-                print("I have Error \(error)")
-            }
+        DispatchQueue.main.async {
+            self.txt = searchBar.searchTextField.text!
+            self.completion?(self.txt)
         }
-}
+       
+//        searhNewsByWord(title: txt)
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+//    func searhNewsByWord(title: String) {
+//        APICaller.shared.searchStories(title: title){ [weak self] result in
+//
+//            switch result {
+//            case.success(let articles):
+//                self?.articles = articles
+//                self?.viewModels = articles.compactMap({
+//                    NewsTableViewCellViewModel(
+//                        title: $0.title,
+//                        subtitle: $0.description ?? "No Description",
+//                        imageURL: URL(string: $0.urlToImage ?? ""),//может сюда вставляется дефолтная картинка?
+//                        published: $0.publishedAt
+//                    )
+//                })
+//
+//                DispatchQueue.main.async {
+//                    print("HOGFKKERFOFKWOFKPWE!!!!!!!!!!!!!!2131231414")
+//                    MainViewController().newsTableView.reloadData()
+//                    MainViewController().title = "\(title) news"
+//                    MainViewController().backbutton.customView?.isHidden = false
+//                }
+//
+//            case.failure( let error ):
+//                print("I have Error \(error)")
+//            }
+//        }
+//    }
     
     
-
+    
     
 }
